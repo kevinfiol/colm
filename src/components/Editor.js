f73896581799d200449c6592a34e68ae1397697b
kevinfiol <me@kevinfiol.com>
2020-09-23 02:09:35 -0400

    2.2.0: Add sublime keybindings

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -3,6 +3,8 @@ import b from 'bss';
 import CodeMirror from 'CodeMirror';
 import markdown from 'CodeMirror/mode/markdown/markdown';
 import css from 'CodeMirror/mode/css/css';
+import comment from 'CodeMirror/addon/comment/comment';
+import keymap from 'CodeMirror/keymap/sublime';
 
 const StyledCode =
     'div.code' +
@@ -24,7 +26,8 @@ const Editor = {
                     value: editorContent,
                     mode: syntax,
                     lineNumbers: true,
-                    theme: 'base16-dark'
+                    theme: 'base16-dark',
+                    keyMap: 'sublime'
                 });
 
                 cm.on('change', cm => {
